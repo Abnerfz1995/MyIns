8d8af9c39f71137f182ef5e01b8b1337007c4e7e
Zheng Fu <abnerfz@dyn-160-39-211-56.dyn.columbia.edu>
2019-10-17 15:18:28 -0400

    3rd try

--- MyInsta/settings.py
+++ MyInsta/settings.py
@@ -132,3 +132,5 @@ LOGOUT_REDIRECT_URL = 'posts'
 AUTH_USER_MODEL = 'Insta.MyInstaUser'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
